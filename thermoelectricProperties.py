58e6f7e99a62adab2c1e4a54588e753f96618070
S. Aria Hosseini <aria.hosseini73@gmail.com>
2020-08-17 15:35:25 -0700

    Add files via upload

--- thermoelectricProperties.py
+++ thermoelectricProperties.py
@@ -16,6 +16,8 @@ from mpl_toolkits import mplot3d
 from matplotlib.colors import LightSource
 import seaborn as sns
 from accum import accum
+from skimage import measure
+from skimage.draw import ellipsoid
 
 sns.set()
 sns.set_context("paper", font_scale=2, rc={"lines.linewidth": 4})
@@ -228,7 +230,6 @@ class thermoelectricProperties:
             int_ = np.trapz(f, t, axis=1)
             tau[r_idx] = 1 / (N[r_idx] / (2 * np.pi)**3 * int_) * thermoelectricProperties.e2C
         Ec, indices, return_indices = np.unique(E, return_index=True, return_inverse=True)
-        print("Ec",Ec[30:])
         tau_c = np.empty((len(ro), len(indices)))
         tauFunctionEnergy = np.empty((len(ro), len(energyRange[0])))
         for r_idx in np.arange(len(ro)):
@@ -237,6 +238,96 @@ class thermoelectricProperties:
             ESpline = PchipInterpolator(Ec[30:], tau_c[tau_idx,30:])
             tauFunctionEnergy[tau_idx] = ESpline(energyRange)
         return tauFunctionEnergy
+
+    def tau3D_spherical(self,energyRange, nk, Uo, m, vfrac, valley, dk_len, ro, n=32):
+        meff = np.array(m) * thermoelectricProperties.me
+        ko = 2 * np.pi / self.latticeParameter * np.array(valley)
+        del_k = 2*np.pi/self.latticeParameter * dk_len * np.array([1, 1, 1])
+        N = 3*vfrac/4/np.pi/ro**3
+        kx = np.linspace(ko[0], ko[0] + del_k[0], nk[0], endpoint=True)  # kpoints mesh
+        ky = np.linspace(ko[1], ko[1] + del_k[1], nk[1], endpoint=True)  # kpoints mesh
+        kz = np.linspace(ko[2], ko[2] + del_k[2], nk[2], endpoint=True)  # kpoints mesh
+        [xk, yk, zk] = np.meshgrid(kx, ky, kz)
+        xk_ = np.reshape(xk, -1)
+        yk_ = np.reshape(yk, -1)
+        zk_ = np.reshape(zk, -1)
+        kpoint = np.array([xk_, yk_, zk_])
+        mag_kpoint = norm(kpoint, axis=0)
+        E = thermoelectricProperties.hBar**2 / 2 * ((kpoint[0, :] - ko[0])**2 / meff[0] + (kpoint[1, :] - ko[1])**2 / meff[1] + (kpoint[2, :] - ko[2]) ** 2 / meff[2]) * thermoelectricProperties.e2C
+        scattering_rate = np.zeros((len(ro), len(E)))
+        nu = np.linspace(0, np.pi, n)
+        z_ = -1 * np.cos(nu)
+        r = np.sqrt(1.0 - z_**2)[:, None]
+        theta = np.linspace(0, 2 * np.pi, n)[None, :]
+        x_ = r * np.cos(theta)
+        y_ = r * np.sin(theta)
+        for u in [0,1,2]:
+
+            Q = np.zeros((2 * (n-2) * (n - 1), 3))
+            A = np.zeros((2 * (n-2) * (n - 1), 1))
+            k = 0
+            a_axis = np.sqrt(2 / (thermoelectricProperties.hBar**2 * thermoelectricProperties.e2C) * meff[0] * E[u])
+            b_axis = np.sqrt(2 / (thermoelectricProperties.hBar**2 * thermoelectricProperties.e2C) * meff[1] * E[u])
+            c_axis = np.sqrt(2 / (thermoelectricProperties.hBar**2 * thermoelectricProperties.e2C) * meff[2] * E[u])
+
+            y = -1 * b_axis * y_ + ko[1]
+            x = -1 * a_axis * x_ + ko[0]
+            Z_ = c_axis * z_ + ko[2]
+            z = np.tile(Z_[:, None], (1,n))
+            for j in np.arange(1,n-1):
+                for i in np.arange(2,n):
+                    S = np.array(np.array([x[i,j],y[i,j],z[i,j]])+np.array([x[i-1,j],y[i-1,j],z[i-1,j]])+np.array([x[i-1,j-1],y[i-1,j-1],z[i-1,j-1]]))
+                    Q[k] = S/3
+                    a = norm(np.array([x[i,j],y[i,j],z[i,j]])-np.array([x[i-1,j],y[i-1,j],z[i-1,j]]))
+                    b = norm(np.array([x[i-1,j],y[i-1,j],z[i-1,j]])-np.array([x[i-1,j-1],y[i-1,j-1],z[i-1,j-1]]))
+                    c = norm(np.array([x[i-1,j-1],y[i-1,j-1],z[i-1,j-1]])-np.array([x[i,j],y[i,j],z[i,j]]))
+                    s = a+b+c
+                    s = s/2
+                    A[k] = np.sqrt(s*(s-a)*(s-b)*(s-c))
+                    k += 1
+            for j in np.arange(1,n-1):
+                for i in np.arange(1,n-1):
+                    S = np.array([x[i,j-1],y[i,j-1],z[i,j-1]])+np.array([x[i,j],y[i,j],z[i,j]])+np.array([x[i-1,j-1],y[i-1,j-1],z[i-1,j-1]])
+                    Q[k] = S/3
+                    a = norm(np.array([x[i,j-1],y[i,j-1],z[i,j-1]])-np.array([x[i,j],y[i,j],z[i,j]]))
+                    b = norm(np.array([x[i,j],y[i,j],z[i,j]])-np.array([x[i-1,j-1],y[i-1,j-1],z[i-1,j-1]]))
+                    c = norm(np.array([x[i-1,j-1],y[i-1,j-1],z[i-1,j-1]])-np.array([x[i,j-1],y[i,j-1],z[i,j-1]]))
+                    s = a+b+c
+                    s = s/2
+                    A[k] = np.sqrt(s*(s-a)*(s-b)*(s-c))
+                    k += 1
+            for i in np.arange(2,n):
+                S = np.array([x[i,0],y[i,0],z[i,0]])+np.array([x[i-1,0],y[i-1,0],z[i-1,0]])+np.array([x[i-1,-2],y[i-1,-2],z[i-1,-2]])
+                Q[k] = S/3
+                a = norm(np.array([x[i,0],y[i,0],z[i,0]])-np.array([x[i-1,0],y[i-1,0],z[i-1,0]]))
+                b = norm(np.array([x[i-1,0],y[i-1,0],z[i-1,0]])-np.array([x[i-1,-2],y[i-1,-2],z[i-1,-2]]))
+                c = norm(np.array([x[i-1,-2],y[i-1,-2],z[i-1,-2]])-np.array([x[i,0],y[i,0],z[i,0]]))
+                s = a+b+c
+                s = s/2
+                A[k] = np.sqrt(s*(s-a)*(s-b)*(s-c))
+                k += 1
+            for i in np.arange(1,n-1):
+                S = np.array([x[i,-2],y[i,-2],z[i,-2]])+np.array([x[i,0],y[i,0],z[i,0]])+np.array([x[i-1,-2],y[i-1,-2],z[i-1,-2]])
+                Q[k] = S/3
+                a = norm(np.array([x[i,-2],y[i,-2],z[i,-2]])-np.array([x[i,0],y[i,0],z[i,0]]))
+                b = norm(np.array([x[i,0],y[i,0],z[i,0]])-np.array([x[i-1,-2],y[i-1,-2],z[i-1,-2]]))
+                c = norm(np.array([x[i-1,-2],y[i-1,-2],z[i-1,-2]])-np.array([x[i,-2],y[i,-2],z[i,-2]]))
+                s = a+b+c
+                s = s/2
+                A[k] = np.sqrt(s*(s-a)*(s-b)*(s-c))
+                k += 1
+        qx = kpoint[0,u] - Q[:,0]
+        qy = kpoint[1,u] - Q[:,1]
+        qz = kpoint[2,u] - Q[:,2]
+        q  = np.sqrt(qx**2+qy**2+qz**2)
+        cosTheta = np.matmul(kpoint[:,u][None,:],Q.T)/norm(kpoint[:,u])/np.sqrt(np.sum(Q**2,axis=1))
+        delE = np.abs(thermoelectricProperties.hBar**2*((Q[:,0]-ko[0])/meff[0]+(Q[:,1]-ko[1])/meff[1]+(Q[:,2]-ko[2])/meff[2]))
+        for ro_idx in  np.arange(len(ro)):
+          M = 4*np.pi*Uo*(1/q*np.sin(ro[ro_idx]*q)-ro[ro_idx]*np.cos(ro[ro_idx]*q))/(q**2)
+          SR = 2*np.pi/thermoelectricProperties.hBar*M*np.conj(M)
+          f = SR/delE*(1-cosTheta);
+          scattering_rate[ro_idx,u] = N[ro_idx]/(2*np.pi)**3*np.sum(f*A.T)
+        return scattering_rate
 
     def electricalProperties(self, E, DoS, vg, Ef, dfdE, Temp, tau):
         X = DoS * vg**2 * dfdE

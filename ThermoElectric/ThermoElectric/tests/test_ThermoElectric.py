dfd7788500637121fd5f2383a66723db86a80f23
Aria Hosseini <aria_h@mit.edu>
2022-03-10 18:39:37 -0800

    Add cookicutter template

---
+++ ThermoElectric/ThermoElectric/tests/test_ThermoElectric.py
@@ -0,0 +1,15 @@
+"""
+Unit and regression test for the ThermoElectric package.
+"""
+
+# Import package, test suite, and other packages as needed
+import sys
+
+import pytest
+
+import ThermoElectric
+
+
+def test_ThermoElectric_imported():
+    """Sample test, will always pass so long as import statement worked."""
+    assert "ThermoElectric" in sys.modules
